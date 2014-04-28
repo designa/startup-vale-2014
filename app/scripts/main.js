b6023f21afb4a4346e9efb9f6f856945cbb82f3d
Michel Allan <michel_allan20xx@hotmail.com>
2014-04-28 18:14:50 -0300

    landing page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,17 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function(){
+	var target = $(window).height()/2;
+
+
+	$(window).scroll(function() {
+		$(".marker").each(function(){
+			var eTop = $(this).offset().top;
+			var elementYPosition = (eTop  - $(window).scrollTop());
+
+			if (elementYPosition <= target) {
+				$(".marker").css("background", "#ddd");
+
+	    	$(this).css("background", "black");
+			};
+		});
+	});
+});
